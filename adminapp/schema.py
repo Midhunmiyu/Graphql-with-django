493129bbc2f618b6f61e7fd6b5f9bfc66af5d3d7
Midhunmiyu <midhunrajagopal112@gmail.com>
2024-06-02 12:51:41 +0530

    initial commit

---
+++ adminapp/schema.py
@@ -0,0 +1,19 @@
+import graphene
+from graphene_django import DjangoObjectType
+from .models import Product
+
+
+class ProductType(DjangoObjectType):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class Query(graphene.ObjectType):
+    all_products = graphene.List(ProductType)
+
+    def resolve_all_products(self, info):
+        return Product.objects.all()
+    
+schema = graphene.Schema(query=Query)
+
+
